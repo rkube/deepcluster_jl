1933e5f1e0e998a86122c7324136966a37493d3b
Ralph Kube <ralph_kube@gmx.net>
2022-11-10 03:13:40 -0500

    Re-initializing classifier weights every epoch

--- src/main.jl
+++ src/main.jl
@@ -2,6 +2,7 @@ using Clustering
 using Flux
 using Flux.Data: DataLoader
 using MLDatasets
+using MLDataPattern
 using Clustering
 using CairoMakie
 using CUDA
@@ -14,7 +15,8 @@ Deep Clustering for Unsupervised Learning of Visual Features
 
 # Load MNIST as a training set
 all_img_x, all_y = MNIST(:train)[:]
-all_img_x = Flux.unsqueeze(all_img_x, 3) |> gpu;
+all_img_x = Flux.unsqueeze(all_img_x, 3);
+all_img_gpu = all_img_x |> gpu;
 
 n_samples = length(all_y);
 # Get ground-truth assignments for MNIST training set
@@ -79,32 +81,60 @@ params = Flux.params(model);
 # Cluster the code
 num_epochs = 20
 NMI_list = zeros(num_epochs);
-batch_losses = zeros(num_batches, num_epochs);
+# We are oversampling and don't know the number of batchs a-priori. Take a large number and cross thumbs.
+batch_losses = zeros(10000, num_epochs);
+
 for epoch ∈ 1:num_epochs
     # Use all model layers but exclude the last relu and the final dense layer
-    features = model[1:10](all_img_x);
+    features = model[1:10](all_img_gpu);
     features_c = features |> cpu;
     cluster_code = kmeans(features_c, 10, maxiter=500);
     class_counts= [(i, count(==(i), cluster_code.assignments)) for i ∈ 1:10]
     NMI_list[epoch] = mutualinfo(cluster_code, truth)
     @show epoch, NMI_list[epoch], class_counts
     # Construct a new dataloader.
-    labels = Flux.onehotbatch(cluster_code.assignments, 1:10) |> gpu;
-    data_loader = DataLoader((all_img_x, labels); batchsize=batch_size, shuffle=true)
+    #labels = Flux.onehotbatch(cluster_code.assignments, 1:10) |> gpu;
+    #labels_gpu = cluster_code.assignments |> gpu;
+
+    # Over-sample the images so that each cluster has an identical amount of
+    # samples for training
+    # Oversample has issues with onehotbatch.
+    # In particular if we pass a onehotbatch as an argument it will return a bool matrix.
+    # So instead of passing a onehotbatch, we just submit the cluster_code.assignment matrix.
+    # Then convert the matrix into a one-hot batch in the iteration over the data_loader.
+    img_os, labels_os = oversample((all_img_x, cluster_code.assignments));
+
+    # Create full arrays with the oversampled data. This runs much faster
+    # than repeated calls to oversampled array.
+    img_os_gpu = similar(img_os);
+    img_os_gpu[:] = img_os[:]
+    img_os_gpu = img_os_gpu |> gpu;
+    labels_full = similar(labels_os);
+    labels_full[:] = labels_os[:]
+    #labels_os_gpu = labels_os_gpu |> gpu;
+
+    data_loader = DataLoader((img_os_gpu, labels_os); batchsize=batch_size, shuffle=true)
+
+    ### We need to re-initialize the weights of the top layer here.
+    model[end].weight[:,:] .= Flux.glorot_uniform(size(model[end].weight)...) |> gpu;
+    model[end].bias .= 0f0
 
     # Train the model in the data.
     ix = 1
     for (X, Y) in data_loader
+        Y_1h = Flux.onehotbatch(Y, 1:10) |> gpu;
         # Store current loss in array. This can't be in gradient call
-        batch_losses[ix, epoch] = Flux.Losses.crossentropy(softmax(model(X)), Y)
+        batch_losses[ix, epoch] = Flux.Losses.crossentropy(softmax(model(X)), Y_1h)
+        #@show ix, batch_losses[ix, epoch]
         ix += 1
 
         grads = Flux.gradient(params) do 
-            batch_loss = Flux.Losses.crossentropy(softmax(model(X)), Y) + λ * sum(sqnorm, params);
+            batch_loss = Flux.Losses.crossentropy(softmax(model(X)), Y_1h) + λ * sum(sqnorm, params);
         end
         Flux.Optimise.update!(opt, params, grads)
     end
-    f, a, l = lines(batch_losses[:, epoch]; axis=(; xlabel="batch", ylabel="loss", title="epoch $(epoch)"))
+    plot_idx = batch_losses[:, epoch] .> 0.0
+    f, a, l = lines(batch_losses[plot_idx, epoch]; axis=(; xlabel="batch", ylabel="loss", title="epoch $(epoch)"))
     save("loss_$(epoch).png", f)
 
     # ### Now evaluate the normalized mutual information
